b47873b7b3f676b2a2a97363348b9a7bb6963ce1
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-10-10 16:05:02 +0200

    Git init

---
+++ app/src/main/java/com/mp08/p01calculadoracriptomonedes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mp08.p01calculadoracriptomonedes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
